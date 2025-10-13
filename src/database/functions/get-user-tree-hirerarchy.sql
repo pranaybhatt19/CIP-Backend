d34b2588df0c315cf05de3872650572dc6b0746d
sfxhchauhan <hchauhan@seamfix.com>
2025-10-13 17:18:50 +0530

    updated experience years data type

--- src/database/functions/get-user-tree-hirerarchy.sql
+++ src/database/functions/get-user-tree-hirerarchy.sql
@@ -27,7 +27,7 @@ RETURNS TABLE(
     email text,
     reporting_person json,
     designation json,
-    experience_years numeric,
+    experience_years text,
     attempts_count bigint,
     medium_of_education text,
     last_communication_date timestamp,
@@ -49,8 +49,11 @@ BEGIN
             d.name::text AS designation_name,
             u.medium_of_education::text,
             u.is_active,
-            (DATE_PART('year', AGE(NOW(), u.experience)) 
-                      + DATE_PART('month', AGE(NOW(), u.experience)) / 100)::numeric AS experience_years
+            TO_CHAR(
+                DATE_PART('year', AGE(NOW(), u.experience))
+                + DATE_PART('month', AGE(NOW(), u.experience)) / 100.0
+                , 'FM999990D00'
+            ) AS experience_years
         FROM cip_schema.users u
         LEFT JOIN cip_schema.users rp ON rp.id = u.reporting_person_id
         LEFT JOIN cip_schema.designations d ON d.id = u.designation_id
@@ -68,8 +71,11 @@ BEGIN
             d.name::text AS designation_name,
             u.medium_of_education::text,
             u.is_active,
-            (DATE_PART('year', AGE(NOW(), u.experience)) 
-                      + DATE_PART('month', AGE(NOW(), u.experience)) / 100)::numeric AS experience_years
+            TO_CHAR(
+                DATE_PART('year', AGE(NOW(), u.experience))
+                + DATE_PART('month', AGE(NOW(), u.experience)) / 100.0
+                , 'FM999990D00'
+            ) AS experience_years
         FROM cip_schema.users u
         INNER JOIN forward_hierarchy h ON u.reporting_person_id = h.id
         LEFT JOIN cip_schema.users rp ON rp.id = u.reporting_person_id
@@ -107,9 +113,9 @@ BEGIN
             AND (reporting_person_ids IS NULL OR fh.reporting_person_id = ANY(reporting_person_ids))
             AND (
                 experience_type IS NULL OR experience_value IS NULL OR
-                (experience_type = 'LESS_THAN' AND fh.experience_years < experience_value) OR
-                (experience_type = 'GREATER_THAN' AND fh.experience_years > experience_value) OR
-                (experience_type = 'EQUALS' AND fh.experience_years = experience_value)
+                (experience_type = 'LESS_THAN' AND (fh.experience_years::numeric) < experience_value) OR
+                (experience_type = 'GREATER_THAN' AND (fh.experience_years::numeric) > experience_value) OR
+                (experience_type = 'EQUALS' AND (fh.experience_years::numeric) = experience_value)
             )
             AND (
                fh.reporting_person_id IS NOT NULL OR 
@@ -144,9 +150,11 @@ BEGIN
             u.designation_id,
             d.name::text AS designation_name,
             u.medium_of_education::text,
-            u.is_active,
-            (DATE_PART('year', AGE(NOW(), u.experience)) 
-                      + DATE_PART('month', AGE(NOW(), u.experience)) / 100)::numeric AS experience_years
+            TO_CHAR(
+                DATE_PART('year', AGE(NOW(), u.experience))
+                + DATE_PART('month', AGE(NOW(), u.experience)) / 100.0
+                , 'FM999990D00'
+            ) AS experience_years
         FROM cip_schema.users u
         LEFT JOIN cip_schema.users rp ON rp.id = u.reporting_person_id
         LEFT JOIN cip_schema.designations d ON d.id = u.designation_id
@@ -170,8 +178,11 @@ BEGIN
             d.name::text AS designation_name,
             u.medium_of_education::text,
             u.is_active,
-            (DATE_PART('year', AGE(NOW(), u.experience)) 
-                      + DATE_PART('month', AGE(NOW(), u.experience)) / 100)::numeric AS experience_years
+            TO_CHAR(
+                DATE_PART('year', AGE(NOW(), u.experience))
+                + DATE_PART('month', AGE(NOW(), u.experience)) / 100.0
+                , 'FM999990D00'
+            ) AS experience_years
         FROM cip_schema.users u
         INNER JOIN reverse_hierarchy rh ON u.id = rh.reporting_person_id
         LEFT JOIN cip_schema.users rp ON rp.id = u.reporting_person_id
